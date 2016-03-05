f4fea69a9ff43cd961b76ce430e38b5604e356cf
Wyatt Lam <wyatt.lam1@gmail.com>
2016-03-05 13:42:56 -0800

    lazy image loading

--- js/flickrService.js
+++ js/flickrService.js
@@ -73,28 +73,35 @@ function photoByComparingHeight(imgContainerHeight, photoSizes) {
     return photo;
 }
 
-$('.img-container').each(function(i, imgContainer) {
-    var photoID = imgContainer.id;
-    flickrService.getPhotoSizes(photoID, function(photoSizes) {
-        var imgContainerWidth = imgContainer.offsetWidth;
-        var imgContainerHeight = imgContainer.offsetHeight;
-        var photo = "";
-        if (imgContainerWidth < imgContainerHeight) {
-           photo = photoByComparingHeight(imgContainerHeight, photoSizes);
-        } else {
-           photo = photoByComparingWidth(imgContainerWidth, photoSizes);
-        }
+$(document).ready(function() {
+    $('.img-container').each(function(i, imgContainer) {
+        var photoID = imgContainer.id;
+        flickrService.getPhotoSizes(photoID, function(photoSizes) {
+            var imgContainerWidth = imgContainer.offsetWidth;
+            var imgContainerHeight = imgContainer.offsetHeight;
+            var photo = "";
+            if (imgContainerWidth < imgContainerHeight) {
+               photo = photoByComparingHeight(imgContainerHeight, photoSizes);
+            } else {
+               photo = photoByComparingWidth(imgContainerWidth, photoSizes);
+            }
 
-        // set lightbox link
-        $(imgContainer.children[0]).attr("href", photoSizes.size[9].source);
+            // set lightbox link
+            $(imgContainer.children[0]).attr("href", photoSizes.size[9].source);
 
-        // set actual image
-        // div background images require at least width or height to be set.
-        //   In this case, we have a width of 100% and a height of 'auto', which doesn't render.
-        //   The solution is to force a padding of the width to height ratio.
-        if ($(imgContainer).hasClass('width100')) {
-            $(imgContainer).css('cssText', "padding-top:" + (photo.height/photo.width * 100) + '% !important');
-        }
-        $(imgContainer).css('background-image', "url('" + photo.source + "')");
+            // set actual image
+            // div background images require at least width or height to be set.
+            //   In this case, we have a width of 100% and a height of 'auto', which doesn't render.
+            //   The solution is to force a padding of the width to height ratio.
+            if ($(imgContainer).hasClass('width100')) {
+                $(imgContainer).css('cssText', "padding-top:" + (photo.height/photo.width * 100) + '% !important');
+            }
+
+            $(imgContainer).attr("data-original", photo.source)
+            $(imgContainer).lazyload({ 
+                threshold : 500,
+                effect : "fadeIn" 
+            })
+        });
     });
-});    
+});
