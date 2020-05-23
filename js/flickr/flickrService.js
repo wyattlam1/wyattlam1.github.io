c71e24e1ae01153a44402ba68c8d3bfcb5a4831c
Wyatt Lam <wyatt.lam1@gmail.com>
2020-05-23 08:34:20 -0700

    Separate FlickrService into FlickrConfig

--- js/flickr/flickrService.js
+++ js/flickr/flickrService.js
@@ -1,69 +1,91 @@
-//import fetch from 'node-fetch';
+import { FlickrConfig } from './flickrConfig.js';
 
-const FLICKR_API_KEY = "e62f00df5c9e726baeea4bf05e956f98";
-const WYATTLAM_USER_ID = '95913990@N07';
-const BEST_OF_ALBUMS = {
-    2016: "72157675078787323"
-};
+function photoByComparingWidth(imgContainerWidth, photoSizes) {
+    var photo = "";
+    if (imgContainerWidth <= 240) {
+        photo = photoSizes.size[3];
+    } else if (imgContainerWidth <= 320) {
+        photo = photoSizes.size[4];
+    } else if (imgContainerWidth <= 500) {
+        photo = photoSizes.size[5];
+    } else if (imgContainerWidth <= 640) {
+        photo = photoSizes.size[6];
+    } else if (imgContainerWidth <= 800) {
+        photo = photoSizes.size[7];
+    } else if (imgContainerWidth <= 1024) {
+        photo = photoSizes.size[8];
+    } else {
+        photo = photoSizes.size[9];
+    }
+    return photo;
+}
 
-const FlickrService = function () {
-    const getPhotoSizes = function (photoId, callback) {
-        const url = new URL("https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&format=json&nojsoncallback=1");
-        const params = {
-            'api_key': FLICKR_API_KEY,
-            'photo_id': photoId,
-        };
-        Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
+function photoByComparingHeight(imgContainerHeight, photoSizes) {
+    var photo = ""
+    if (imgContainerHeight <= 180) {
+        photo = photoSizes.size[3];
+    } else if (imgContainerHeight <= 240) {
+        photo = photoSizes.size[4];
+    } else if (imgContainerHeight <= 375) {
+        photo = photoSizes.size[5];
+    } else if (imgContainerHeight <= 480) {
+        photo = photoSizes.size[6];
+    } else if (imgContainerHeight <= 600) {
+        photo = photoSizes.size[7];
+    } else if (imgContainerHeight <= 768) {
+        photo = photoSizes.size[8];
+    } else {
+        photo = photoSizes.size[9];
+    }
+    return photo;
+}
 
+const FlickrService = () => {
+    const getPhotoSizes = (photoId, onComplete) => {
+        const url = FlickrConfig().getPhotoSizesUrl(photoId);
         fetch(url)
             .then((response) => {
                 if (response.ok) {
                     response.json().then((json) => {
-                        callback(json.sizes);
+                        onComplete(json.sizes);
                     });
                 } else {
                     console.log(response);
                 }
             });
     }
+    return { getPhotoSizes };
+}
 
-    // API Docs: https://www.flickr.com/services/api/flickr.photosets.getPhotos.htm
-    const getAlbum = function (photoSetId, onComplete) {
-        const url = new URL('https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&format=json&nojsoncallback=1');
-        const params = {
-            'api_key': FLICKR_API_KEY,
-            'user_id': WYATTLAM_USER_ID,
-            'photoset_id': photoSetId,
-            'privacy_filter': '1',
-            'extras': 'url_s',
-        }
+$(document).ready(function () {
+    $('.img-container').each(function (i, imgContainer) {
+        var photoID = imgContainer.id;
+        FlickrService().getPhotoSizes(photoID, function (photoSizes) {
+            var imgContainerWidth = imgContainer.offsetWidth;
+            var imgContainerHeight = imgContainer.offsetHeight;
+            var photo = "";
+            if (imgContainerWidth < imgContainerHeight) {
+                photo = photoByComparingHeight(imgContainerHeight, photoSizes);
+            } else {
+                photo = photoByComparingWidth(imgContainerWidth, photoSizes);
+            }
 
-        Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
+            // set lightbox link
+            $(imgContainer.children[0]).attr("href", photoSizes.size[9].source);
 
-        fetch(url)
-            .then((response) => {
-                if (response.ok) {
-                    response.json().then((json) => {
-                        onComplete({
-                            title: json.photoset.title,
-                            photoIds: json.photoset.photo.map((photo) => {
-                                return {
-                                    id: photo.id,
-                                    thumbnailUrl: photo.url_s
-                                }
-                            }),
-                        });
-                    });
-                } else {
-                    console.log(response);
-                }
-            });
-    }
+            // set actual image
+            // div background images require at least width or height to be set.
+            //   In this case, we have a width of 100% and a height of 'auto', which doesn't render.
+            //   The solution is to force a padding of the width to height ratio.
+            if (imgContainerHeight == 0) {
+                $(imgContainer).css('cssText', "padding-top:" + (photo.height / photo.width * 100) + '% !important');
+            }
 
-    return {
-        getPhotoSizes,
-        getAlbum,
-    };
-}();
-
-export { FlickrService };
+            $(imgContainer).attr("data-original", photo.source)
+            $(imgContainer).lazyload({
+                threshold: 500,
+                effect: "fadeIn"
+            })
+        });
+    });
+});
--- js/flickr/generateFlickrBestofAlbumPost.js
+++ js/flickr/generateFlickrBestofAlbumPost.js
@@ -1,4 +1,5 @@
-import { FlickrService } from './flickrService.js';
+import { FlickrConfig } from './flickrConfig.js';
+import fetch from 'node-fetch';
 
 const twoByTwoTemplate = '{% include photo-story-2x1-50-50.html photo1="{0}" photo2="{1}" %}'
 
@@ -11,19 +12,45 @@ String.prototype.format = function () {
     return content;
 };
 
-FlickrService.getAlbum('72157675078787323', (response) => {
-    const photoIds = response.photoIds;
-    const result = [];
-    var i = 0;
-    while (i < photoIds.length) {
-        if (i + 1 < photoIds.length) {
-            result.push(twoByTwoTemplate.format(photoIds[i].id, photoIds[i + 1].id));
-        } else {
-            result.push(twoByTwoTemplate.format(photoIds[i].id, "0"));
-        }
-        i += 2;
-    }
+const fetchPhotoIds = (albumId, onComplete) => {
+    const url = FlickrConfig().getAlbumUrl(albumId);
+    fetch(url)
+        .then((response) => {
+            if (response.ok) {
+                response.json().then((json) => {
+                    onComplete({
+                        title: json.photoset.title,
+                        photoIds: json.photoset.photo.map((photo) => {
+                            return {
+                                id: photo.id,
+                                thumbnailUrl: photo.url_s
+                            }
+                        }),
+                    });
+                });
+            } else {
+                console.log(response);
+            }
+        });
+}
 
+const generateAlbum = (albumId) => {
+    fetchPhotoIds(
+        albumId,
+        (response) => {
+            const photoIds = response.photoIds;
+            const result = [];
+            var i = 0;
+            while (i < photoIds.length) {
+                if (i + 1 < photoIds.length) {
+                    result.push(twoByTwoTemplate.format(photoIds[i].id, photoIds[i + 1].id));
+                } else {
+                    result.push(twoByTwoTemplate.format(photoIds[i].id, "0"));
+                }
+                i += 2;
+            }
+            console.log(result.join("\n"));
+        });
+}
 
-    console.log(result.join("\n"));
-});
+generateAlbum('72157675078787323');
